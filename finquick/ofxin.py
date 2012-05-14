60f47e113f3a54d257908694b833101c0dfd2176
Dan Connolly <http://www.madmode.com/>
2012-05-13 21:17:59 -0500

    ofxin works for a fairly complete example/doctest   - split prepare() out of run()   - add online_id when we import so that re-import avoids dups   - include ledgerbal in summary   - fix end-tag handling   - use bias-100 Integer rather than DECIMAL in StmtTrn table

--- finquick/ofxin.py
+++ finquick/ofxin.py
@@ -1,71 +1,4 @@
-'''
->>> import pkg_resources
->>> ofxin = pkg_resources.resource_stream(__name__, 'test/test_ofx.ofx')
-
->>> p = OFXParser()
->>> list(p.header(ofxin))
-... #doctest: +NORMALIZE_WHITESPACE
-[('OFXHEADER', '100'), ('DATA', 'OFXSGML'), ('VERSION', '102'),
- ('SECURITY', 'NONE'), ('ENCODING', 'USASCII'),
- ('CHARSET', '1252'), ('COMPRESSION', 'NONE'),
- ('OLDFILEUID', 'NONE'), ('NEWFILEUID', 'NONE')]
-
-.. todo:: decode according to charset
-
->>> body = ''.join([line for line in ofxin])
->>> p.feed(body)
->>> p.root.tag
-'ofx'
-
->>> len(p.root.findall('.//stmttrn'))
-2
-
->>> [field.tag for field in p.root.find('.//stmttrn')]
-... #doctest: +NORMALIZE_WHITESPACE
-['trntype', 'dtposted', 'trnamt', 'fitid', 'name',
- 'checknum', 'payeeid', 'memo']
-
->>> [(t.fitid, t.trnamt, t.dtposted)
-...  for t in [dotelt(e) for e in p.root.findall('.//stmttrn')]]
-[('2-6', '60.00', '20110117212742'), ('2-9', '60.00', '20110117221215')]
-
-
-### from xml.etree.ElementTree import tostring
-### print tostring(p.root)
-
-    >>> import pprint
-    >>> pprint.pprint(p.summary())
-    {'acctid': '123456789',
-     'bankid': '123456789',
-     'dtserver': datetime.datetime(2011, 1, 17, 22, 12, 51),
-     'fid': None,
-     'org': 'Bank of America'}
-
-    >>> pprint.pprint(list(p.transactions()))
-    [{'checknum': '2-6',
-      'dtposted': datetime.datetime(2011, 1, 17, 21, 27, 42),
-      'fitid': '2-6',
-      'memo': None,
-      'name': 'Jared Fetter',
-      'payeeid': '2',
-      'trnamt': Decimal('60.00'),
-      'trntype': 'CREDIT'},
-     {'checknum': '2-9',
-      'dtposted': datetime.datetime(2011, 1, 17, 22, 12, 15),
-      'fitid': '2-9',
-      'memo': None,
-      'name': 'Jared Fetter',
-      'payeeid': '2',
-      'trnamt': Decimal('60.00'),
-      'trntype': 'CREDIT'}]
-
->>> logging.basicConfig(level=logging.DEBUG)
->>> logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
->>> (session, i) = models.Mock.make([models.KSession, Importer])
->>> from models import Account
->>> bank = session.query(Account).filter(Account.name == 'Bank X').one()
->>> i.run(p.summary(), p.transactions(), bank, '@@')
-
+'''ofxin -- Parse OFX and import with matching by `online_id`.
 '''
 
 from xml.etree.ElementTree import Element
@@ -77,7 +10,7 @@ import sgmllib  # Note: deprecated
 
 from injector import inject
 from sqlalchemy import Column, and_
-from sqlalchemy.types import String, DECIMAL, DATETIME
+from sqlalchemy.types import String, Integer, DATETIME
 
 import models
 
@@ -85,16 +18,62 @@ log = logging.getLogger(__name__)
 
 
 class Importer(object):
-    @inject(datasource=models.KSession)
-    def __init__(self, datasource):
-        self._ds = datasource
+    r'''Import OFX data into GnuCash account using `online_id` matching.
 
-    def run(self, summary, transactions, acct, txfr):
+    Consider a simple OFX file with two transactions:
+    >>> import pkg_resources
+    >>> ofxin = pkg_resources.resource_stream(__name__, 'test/test_ofx.ofx')
+
+    Parse the data:
+    >>> summary, transactions_g = OFXParser.ofx_data(ofxin)
+    >>> transactions = list(transactions_g)
+
+    Choose the destination account; note splits before import:
+    >>> (session, i) = models.Mock.make([models.KSession, Importer])
+    >>> from models import Account, Split
+    >>> bank = session.query(Account).\
+    ...        filter(Account.name == 'Bank X').one()
+    >>> [split.value_num
+    ...  for split in session.query(Split).filter(Split.account == bank)]
+    [-25000]
+
+    This simple import uses one transfer account for all new transactions:
+    >>> exp = session.query(Account).\
+    ...        filter(Account.name == 'Utilities').one()
+
+    Insert the transactions into a temporary table and match them
+    against existing splits/transactions:
+    ### logging.basicConfig(level=logging.INFO)
+    ### logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+    >>> i.prepare(summary, transactions, bank)
+
+    Then import the unmatched transactions and note the results:
+    >>> i.run(bank, exp, 'currency_guid@@')
+    >>> [split.value_num
+    ...  for split in session.query(Split).filter(Split.account == bank)]
+    [-25000, 6000, 6000]
+
+    Import is idempotent; doing it again has no effect:
+    >>> i.prepare(summary, transactions, bank)
+    >>> i.run(bank, exp, 'currency_guid@@')
+    >>> [split.value_num
+    ...  for split in session.query(Split).filter(Split.account == bank)]
+    [-25000, 6000, 6000]
+
+    '''
+    @inject(datasource=models.KSession,
+            uuidgen=models.KUUIDGen)
+    def __init__(self, datasource, uuidgen):
+        self._ds = datasource
+        self._uuidgen = uuidgen
+
+    def prepare(self, summary, transactions, acct):
         '''
         .. todo:: do something with the summary
         '''
         session = self._ds()  # hmm... instantiate this?
         tst = StmtTrn.__table__
+        session.execute(tst.delete())
         session.execute(tst.insert(),
                         list(transactions))
         ta = models.Account.__table__
@@ -107,10 +86,45 @@ class Importer(object):
             select().\
             where(and_(tsl.c.name == 'online_id',
                        ta.c.guid == acct.guid))
-        novel = tst.outerjoin(online_ids, tst.c.fitid == tsl.c.string_val).\
-            select().with_only_columns(tst.columns).\
-            where(tsl.c.string_val == None)
-        session.execute(novel)
+        matches = tst.join(online_ids, tst.c.fitid == tsl.c.string_val).\
+            select().with_only_columns([tst.c.fitid, tsl.c.obj_guid])
+        for fitid, obj_guid in session.execute(matches).fetchall():
+            session.execute(tst.update().values(match_guid=obj_guid).\
+                                where(tst.c.fitid == fitid))
+
+
+    def run(self, acct, txfr, currency_guid):
+        session = self._ds()  # hmm... instantiate this?
+        for ofxtx in session.query(StmtTrn).\
+                filter(StmtTrn.match_guid == None).all():
+            fmt = models.GuidMixin.fmt
+            tx = models.Transaction(guid=fmt(self._uuidgen()),
+                                    currency_guid=currency_guid,
+                                    num=ofxtx.checknum,
+                                    post_date=ofxtx.dtposted,
+                                    description=ofxtx.name)
+            s1 = models.Split(guid=fmt(self._uuidgen()),
+                              tx_guid=tx.guid,
+                              account_guid=acct.guid,
+                              memo=ofxtx.memo,
+                              reconcile_state='n',
+                              value_num=ofxtx.trnamt,
+                              value_denom=100)
+
+            s2 = models.Split(guid=fmt(self._uuidgen()),
+                              tx_guid=tx.guid,
+                              account_guid=txfr.guid,
+                              reconcile_state='n',
+                              value_num=-ofxtx.trnamt,
+                              value_denom=100)
+            online_id = models.TextSlot(obj_guid=s1.guid,
+                                        name='online_id',
+                                        string_val=ofxtx.fitid)
+            session.add(tx)
+            session.add(s1)
+            session.add(online_id)
+            session.add(s2)
+        session.flush()
 
 
 class StmtTrn(models.Base):
@@ -119,28 +133,108 @@ class StmtTrn(models.Base):
     __tablename__ = 'ofx_stmttrn'
     fitid = Column(String(80), primary_key=True)
     checknum = Column(String(80))
-    dtposted = Column(DATETIME)
+    dtposted = Column(DATETIME, nullable=False)
     memo = Column(String(80))
-    name = Column(String(80))
+    name = Column(String(80), nullable=False)
     payeeid = Column(String(80))
-    trnamt = Column(DECIMAL(precision=8, scale=2))
+    trnamt = Column(Integer, nullable=False)  # actual amount * 100
     trntype = Column(String(80))  # todo: enum 'CREDIT', ...
-    
+    match_guid = Column(String(32))
+
 
 class OFXParser(sgmllib.SGMLParser):
-    '''
+    '''Parse OFX into ElementTree structure; extract summary and transactions.
+
+    >>> import pkg_resources
+    >>> ofxin = pkg_resources.resource_stream(__name__, 'test/test_ofx.ofx')
+    >>> summary, transactions = OFXParser.ofx_data(ofxin)
+
+    >>> import pprint
+    >>> pprint.pprint(summary)
+    {'acctid': '123456789',
+     'balamt': '120.00',
+     'bankid': '123456789',
+     'dtasof': '20110117221251',
+     'dtserver': datetime.datetime(2011, 1, 17, 22, 12, 51),
+     'fid': '5959',
+     'org': 'Bank of America'}
+
+    >>> pprint.pprint(list(transactions))
+    [{'checknum': '2-6',
+      'dtposted': datetime.datetime(2011, 1, 17, 21, 27, 42),
+      'fitid': '2-6',
+      'memo': '90853',
+      'name': 'Jared Fetter',
+      'payeeid': '2',
+      'trnamt': 6000,
+      'trntype': 'CREDIT'},
+     {'checknum': '2-9',
+      'dtposted': datetime.datetime(2011, 1, 17, 22, 12, 15),
+      'fitid': '2-9',
+      'memo': '90853',
+      'name': 'Jared Fetter',
+      'payeeid': '2',
+      'trnamt': 6000,
+      'trntype': 'CREDIT'}]
+
     .. todo:: parser to take a list of tags that should be collected,
               with corredponding callbacks. (also: look into monads
               vs callbacks). yield tablename, record
+
     '''
 
     @classmethod
-    def header(self, lines):
+    def ofx_data(self, ofxin):
+        p = OFXParser()
+        p.header(ofxin)  # discard header
+        body = ''.join([line for line in ofxin])
+        p.feed(body)
+        return p.summary(), p.transactions()
+        
+    @classmethod
+    def header(cls, lines):
         for line in lines:
             if not line.strip():
                 break
             n, v = line.strip().split(':', 1)
             yield n, v
+
+    @classmethod
+    def whitebox(cls):
+        '''
+        >>> import pkg_resources
+        >>> ofxin = pkg_resources.resource_stream(__name__, 'test/test_ofx.ofx')
+        >>> p = OFXParser()
+        >>> list(p.header(ofxin))
+        ... #doctest: +NORMALIZE_WHITESPACE
+        [('OFXHEADER', '100'), ('DATA', 'OFXSGML'), ('VERSION', '102'),
+         ('SECURITY', 'NONE'), ('ENCODING', 'USASCII'),
+         ('CHARSET', '1252'), ('COMPRESSION', 'NONE'),
+         ('OLDFILEUID', 'NONE'), ('NEWFILEUID', 'NONE')]
+
+        .. todo:: decode according to charset
+
+        >>> body = ''.join([line for line in ofxin])
+        >>> p.feed(body)
+        >>> p.root.tag
+        'ofx'
+
+        >>> len(p.root.findall('.//stmttrn'))
+        2
+
+        >>> [field.tag for field in p.root.find('.//stmttrn')]
+        ... #doctest: +NORMALIZE_WHITESPACE
+        ['trntype', 'dtposted', 'trnamt', 'fitid', 'name',
+         'checknum', 'payeeid', 'memo']
+
+        >>> [(t.fitid, t.trnamt, t.dtposted)
+        ...  for t in [dotelt(e) for e in p.root.findall('.//stmttrn')]]
+        [('2-6', '60.00', '20110117212742'), ('2-9', '60.00', '20110117221215')]
+
+        ### from xml.etree.ElementTree import tostring
+        ### print tostring(p.root)
+        '''
+        pass
 
     def __init__(self):
         sgmllib.SGMLParser.__init__(self)
@@ -153,10 +247,7 @@ class OFXParser(sgmllib.SGMLParser):
         log.debug('tag: %s in %s', tag, debugstack)
 
         if self.root is not None:
-            text = ''.join(self._data).strip()
-            if text:
-                current = self._ancestors[-1]
-                current.text = text
+            if self._poptext():
                 self._ancestors.pop()
 
         self._data = []
@@ -171,7 +262,16 @@ class OFXParser(sgmllib.SGMLParser):
         self._ancestors.append(start)
 
     def unknown_endtag(self, tag):
+        self._poptext()
         self._ancestors.pop()
+
+    def _poptext(self):
+        text = ''.join(self._data).strip()
+        if text:
+            current = self._ancestors[-1]
+            current.text = text
+            return True
+        return False
 
     def handle_data(self, data):
         self._data.append(data)
@@ -188,11 +288,14 @@ class OFXParser(sgmllib.SGMLParser):
 
         acct = dotelt(_or(self.root.find('.//bankacctfrom'),
                           self.root.find('.//ccacctfrom')))
+        ledgerbal = dotelt(self.root.find('.//ledgerbal'))
         return dict(dtserver=self.parse_date(root.find('.//dtserver').text),
                     org=fi.org,
                     fid=fi.fid,
                     acctid=acct.acctid,
-                    bankid=acct.get('bankid'))
+                    bankid=acct.get('bankid'),
+                    balamt=ledgerbal.balamt,
+                    dtasof=ledgerbal.dtasof)
 
     def transactions(self):
         root = self.root
@@ -203,7 +306,7 @@ class OFXParser(sgmllib.SGMLParser):
                        dtposted=self.parse_date(tx.dtposted),
                        checknum=tx.get('checknum'),
                        trntype=tx.trntype,
-                       trnamt=Decimal(tx.trnamt),
+                       trnamt=int(Decimal(tx.trnamt)*100),
                        name=tx.name,
                        payeeid=tx.get('payeeid'),
                        memo=tx.get('memo'))
