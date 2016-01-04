4b2d110522ee7ff6a87d957f0a653b207c3fe72e
Dan Connolly <http://www.madmode.com/>
2016-01-03 18:47:08 -0600

    capper: compute balance of account (and its subaccounts) since some date
      - compute subAccounts recursively
      - refactor makeWithConnection as makeDB with .query() method as well
      - take acct name, since from CLI (argv) as well as dbName
        - pass argv, env, and spawn to main() in one process object
        - factor out 'localhost' as host
      - refactor makeSecretLookup as makeSecretTool
      - localize node-keychain ack to makeSecretTool
    
    --HG--
    branch : capper

--- take2js/Capper/apps/ofxies/server/budget.js
+++ take2js/Capper/apps/ofxies/server/budget.js
@@ -1,34 +1,39 @@
-// cribbed from https://github.com/drudge/node-keychain/blob/master/keychain.js
-
 var Q = require('q');
 
-function integrationTestMain(argv, user, spawn, mysql) {
-    var dbName = argv[2];
+function integrationTestMain(process, mysql) {
+    var argv = process.argv,
+        dbName = argv[2], host = 'localhost',
+        acctName = argv[3], since = argv[4];
 
-    var lookup = makeSecretLookup(spawn);
-
-    lookup({
-        'protocol': 'mysql',
-        'server': 'localhost',
-        'object': dbName
+    makeSecretTool(process.spawn).lookup({
+        protocol: 'mysql',
+        server: host,
+        object: dbName
     }).then(function(password) {
-        var withConnection = makeWithConnection(
+        var budget = makeBudget(makeDB(
             mysql,
             {
-                host     : 'localhost',
-                user     : user,
+                host     : host,
+                user     : process.env.LOGNAME,
                 password : password,
                 database : dbName
-            });
+            }));
 
-        getBalance(withConnection).then(function(b) {
-            console.log('Balance: ', b);
+        /*
+        budget.subAccounts(acctName).then(function(accts) {
+            console.log('subaccounts: ', accts);
         });
+        */
+
+        budget.acctBalance(acctName, since).then(function(info) {
+            console.log('balance: ', info.balance);
+        }).done();
     });
 }
 
 
-function makeSecretLookup(spawn) {
+function makeSecretTool(spawn) {
+    // cribbed from https://github.com/drudge/node-keychain/blob/master/keychain.js
     var toolPath = 'secret-tool';
 
     function lookup(what) {
@@ -37,6 +42,7 @@ function makeSecretLookup(spawn) {
             args.push(prop);
             args.push(what[prop]);
         }
+
 
         // console.log('spawn(', toolPath, args, ')');
         var tool = spawn(toolPath, args);
@@ -59,11 +65,14 @@ function makeSecretLookup(spawn) {
         return out.promise;
     }
 
-    return lookup;
+    return Object.freeze({
+        lookup: lookup,
+        path: function() { return toolPath; }
+    });
 }
 
 
-function makeWithConnection(mysql, opts) {
+function makeDB(mysql, opts) {
     function withConnection(fn) {
         var connection = mysql.createConnection(opts);
         connection.connect(); // ???
@@ -72,30 +81,101 @@ function makeWithConnection(mysql, opts) {
         return out;
     }
 
-    return withConnection;
+    function query(dml, params) {
+        return Q.promise(function(resolve, reject) {
+            withConnection(function(c) {
+                // console.log('DEBUG: db.query: ', dml, params || '');
+                c.query(dml, params, function(err, rows) {
+                    if (err) return reject(err);
+                    // console.log('DEBUG: db.query result: ', rows);
+                    resolve(rows);
+                });
+            });
+        });
+    }
+
+    return Object.freeze({
+        withConnection: withConnection,
+        query: query
+    });
 }
 
 
-function getBalance(withConnection) {
-    var out = Q.defer();
 
-    withConnection(function(connection) {
-        connection.query(
-            'SELECT 1 + 1 AS balance',
-            function(err, rows, fields) {
-                if (err) {
-                    out.reject(err);
-                    return;
-                }
-                
-                out.resolve(rows[0].balance);
-            });
+function sqlList(uuids) {
+    // TODO: verify uuid syntax to prevent against SQL injection
+    return uuids.map(function(u) { return '\'' + u + '\''; }).join(', ');
+}
+
+
+function first(rows) {
+    return rows[0];
+}
+
+
+function makeBudget(db) {
+    function subAccounts(acctP) {
+        var q = ('select child.guid, child.name ' +
+                 'from accounts child ' +
+                 'join accounts parent on child.parent_guid = parent.guid ' +
+                 'where parent.guid in (PARENTS)');
+
+        function recur(parents, generations, resolve, reject) {
+            var parentIds = parents.map(function(p) { return p.guid; });
+            db.query(q.replace('PARENTS', sqlList(parentIds))).then(
+                function(children) {
+                    if (children.length == 0) {
+                        var acctIds = [].concat.apply([], generations);
+                        return resolve(acctIds);
+                    }
+                    generations.push(children);
+                    recur(children, generations, resolve, reject);
+                }, reject);
+        }
+
+        return Q.promise(function(resolve, reject) {
+            acctP.then(function(acct) {
+                recur([acct], [[acct]], resolve, reject);
+            }, reject);
+        });
+    }
+
+    function acctBalance(acctName, since) {
+        var q = ('select sum(value_num / value_denom) balance ' +
+                 '  , ? name, ? since ' +
+                 'from splits s ' +
+                 'join accounts a on a.guid = s.account_guid ' +
+                 'join transactions tx on tx.guid = s.tx_guid ' +
+                 'where a.guid in (SUBACCOUNTS) ' +
+                 'and tx.post_date >= ?');
+
+        return subAccounts(acctByName(acctName)).then(function(accts) {
+            var acctIds = accts.map(function(a) { return a.guid; });
+            return db.query(q.replace('SUBACCOUNTS', sqlList(acctIds)),
+                            [acctName, since, since])
+                .then(first);
+        });
+    }
+
+    function acctByName(acctName) {
+        return db.query(
+            'select guid, name from accounts where name = ?',
+            [acctName])
+            .then(first);
+    }
+
+    return Object.freeze({
+        subAccounts: function(acctName) {
+            return subAccounts(acctByName(acctName));
+        },
+        acctBalance: acctBalance
     });
+}
 
-    return out.promise;
-} 
-
-integrationTestMain(process.argv,
-                    process.env.LOGNAME,
-                    require('child_process').spawn,
-                    require('mysql'));
+integrationTestMain(
+    {
+        argv: process.argv,
+        env: process.env,
+        spawn: require('child_process').spawn
+    },
+    require('mysql'));
