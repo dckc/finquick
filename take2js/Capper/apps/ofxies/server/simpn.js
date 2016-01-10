566475b2591ae8b1a70ba9becd790e7dbdd707e2
Dan Connolly <http://www.madmode.com/>
2016-01-10 15:15:48 -0600

    refactor simple.com transaction access as makeSimpleRd().transactions()
    --HG--
    branch : capper

--- take2js/Capper/apps/ofxies/server/simpn.js
+++ take2js/Capper/apps/ofxies/server/simpn.js
@@ -3,44 +3,72 @@ var Q    = require('q');
 function integrationTestMain(env, Nightmare) {
     'use strict';
 
-    const creds = {
-	username: process.env.SIMPLE_USERNAME,
-	password: process.env.SIMPLE_PASSWORD
-    };
+    const credsP = Q({
+        username: env.SIMPLE_USERNAME,
+        password: env.SIMPLE_PASSWORD
+    });
 
-    Q.spawn(() => doit(Nightmare, creds));
+    const acctRd = makeSimpleRd(Nightmare, credsP);
+    acctRd.transactions()
+        .then((txns) => console.log(txns))
+        .then(() => acctRd.end())
+        .done();
 }
 
 
-function* doit(Nightmare, creds) {
+function makeSimpleRd(Nightmare, credsP) {
     'use strict';
-
-    const n = Nightmare({
-	show: true
+    const userAgent = Nightmare({
+        show: true  // DEBUG
     });
 
-    const tx = yield n
-	.goto('https://www.simple.com/signin')
-	.wait(0.5 * 1000)
-	.wait('input#login_username')
-	.type('input#login_username', creds.username)
-	.type('input#login_password', creds.password)
-	.click('input#signin-btn')
-	.wait(0.5 * 1000)
-	.wait('#safe-to-spend')
-        .evaluate(() => Butcher.models.Transactions);
+    const transactions = Q.async(function *(){
+        yield login();
 
-    console.log('Transactions: ', tx);
+        // note document below refers to the user agent's document
+        return yield userAgent
+            .evaluate(() => window.Butcher.models.Transactions);
+    });
 
-    yield n.end();
+    let visited = false;
+
+    const login = Q.async(function*(){
+        const nameSel = '.masthead-username';
+        const getName =
+            () => document.querySelector('.masthead-username').innerText;
+
+        const viz = visited &&
+            (yield userAgent.exists(nameSel)) &&
+            (yield userAgent.visible(nameSel));
+
+        if (! viz) {
+            const creds = yield credsP;
+
+            yield userAgent
+                .goto('https://www.simple.com/signin')
+                .wait(0.5 * 1000)
+                .wait('input#login_username')
+                .type('input#login_username', creds.username)
+                .type('input#login_password', creds.password)
+                .click('input#signin-btn')
+                .wait(0.5 * 1000)
+                .wait(nameSel);
+            visited = true;
+        }
+        const name = yield userAgent.evaluate(getName);
+        return name;
+    });
+
+    return Object.freeze({
+        transactions: transactions,
+        login: login,
+        end: () => userAgent.end()
+    });
 }
 
 
 if (process.env.TESTING) {
     integrationTestMain(
-	{
-	    username: process.env.SIMPLE_USERNAME,
-	    password: process.env.SIMPLE_PASSWORD
-	},
-	require('nightmare'));
+        process.env,
+        require('nightmare'));
 }
