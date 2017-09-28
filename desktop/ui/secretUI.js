e0b321a7a6e5f7f7f9455649dbbb919c2b911d28
Dan Connolly <http://www.madmode.com/>
2017-09-27 21:24:58 -0500

    secretSpace UI: bootstrap style; refactor JS a bit

--- desktop/ui/secretUI.js
+++ desktop/ui/secretUI.js
@@ -10,26 +10,37 @@ import Bacon from 'baconjs';
 
 export function ui(secretSpace /* :SecretSpace */, $ /*: JQuery*/) {
     console.log('@@@building UI...', $);
+    spaceMaker($, attrs => secretSpace.post('subSpace', attrs));
+    searchUI($, () => secretSpace.post('search'));
+}
 
-    $('#items').html('@@@HELLO WORLD.');
+function spaceMaker($, subSpace) {
+    $('#subSpace').asEventStream('click').log('click')
+        .map(_ => ({key: $('#attribute').val(),
+                    value: $('#value').val()})).log('input')
+        .flatMap(attr => Bacon.fromPromise(subSpace({[attr.key]: attr.value})).log('subSpace')
+                 .zip(Bacon.once(attr), (s, a) => [s, a])).log('flatMap / zip')
+        .onValue(([subSpace, attr]) => go(subSpace, attr));
 
+    function go(subSpace, attr) {
+        $('#child').html(
+            $('<iframe />',
+              {src: subSpace.webkey,
+               class: "embed-responsive-item"}));
+        $('#subSpaces').append(
+            $('<a />',
+              {href: subSpace.webkey,
+               text: `${attr.key} = ${attr.value}`})
+                .wrap('<li />').parent())
+    }
+}
+
+
+function searchUI($, search) {
     $('#search').asEventStream('click')
-        .flatMap(_ => Bacon.fromPromise(secretSpace.post('search')))
+        .flatMap(_ => Bacon.fromPromise(search()))
         .map(itemsInfo => itemsInfo.map(i => $('<li />', {text: i['Label']})))
         .onValue(itemsElts => {
             $('#items').html(itemsElts)
-        });
-
-    function attrs() {
-        const attr = $('#attribute').val();
-        const val = $('#value').val();
-        return {[attr]: val}
-    }
-    
-    $('#subSpace').asEventStream('click')
-        .flatMap(_ => Bacon.fromPromise(secretSpace.post('subSpace', attrs())))
-        .onValue(subSpace => {
-            console.log('@@subSpace:', subSpace);
-            $('#subSpaces').append($('<a />', {href: subSpace.webkey, text: '@@TODO'}))
         });
 }
