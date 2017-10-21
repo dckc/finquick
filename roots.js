ad315befb52c9f913af31dd449c806b3edf076c3
Dan Connolly <http://www.madmode.com/>
2017-10-21 16:20:12 -0500

    roots.js seems to work

--- roots.js
+++ roots.js
@@ -2,25 +2,58 @@
 
 @flow
 */
-/*global require, module */
+/*global require, module, console */
 
 'use strict';
 
 const Q = require('q');
 
 /*::
-import typeof readFile from 'fs';
+import {typeof readFile as readFile_t} from 'fs';
 */
 
-function main(fs_readFile /*: readFile*/) {
-    Q.nfcall(fs_readFile, 'capper.db', {encoding: 'utf8'})
-        .then(data => {
-            console.log(data);
-        })
+const CAPPER_DB = 'capper.db';
+
+
+function main(readFile /*: readFile_t*/,
+              stdout /*: typeof process.stdout */) {
+    Q.nfcall(readFile, CAPPER_DB, {encoding: 'utf8'})
+        .then(text => showRoots(JSON.parse(text)))
         .done();
+
+    function showRoots(db) {
+        console.error(`db keys: ${Object.keys(db).length}`);
+        const roots = dbRoots(db);
+        console.error(`roots: ${roots.length}`);
+        roots.forEach(o => {
+            stdout.write(`${JSON.stringify(o)}\n`);
+        });
+    }
 }
+
+function dbRoots(db /*: {string: Object} */) /*: Array<[string, Object]>*/ {
+    const setDiff = (a, b) => a.filter(v => ! b.includes(v));
+
+    return setDiff(Object.keys(db), refs(db)).map(k => [k, db[k].reviver]);
+}
+
+function refs(value /*:any*/) /*: Array<string>*/ {
+    const flatten = arrays => [].concat.apply([], arrays);
+
+    if (value === null || typeof value !== 'object') {
+        return [];
+    } else {
+        if ('@id' in value) {
+            return [value['@id']];
+        } else {
+            return flatten(Object.keys(value).map(k => refs(value[k])));
+        }
+    }
+}
+
 
 if (require.main == module) {
     // Access ambient stuff only when invoked as main module.
-    main(require('fs').readFile);
+    main(require('fs').readFile,
+         process.stdout);
 }
