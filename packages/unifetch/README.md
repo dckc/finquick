fbf51945a63e6fb594b31546d9ad35767b515192
Dan Connolly <dckc@madmode.com>
2021-01-03 18:56:01 -0600

    unifetch: fetch position, transaction data

--- packages/unifetch/README.md
+++ packages/unifetch/README.md
@@ -13,3 +13,15 @@ Stretch goal: Compute capital gains for UniSwap trades.
 5.  Calculate gains / losses
 
 ## Platform: Browser?
+
+## References
+
+- [Uniswap Info](https://info.uniswap.org/accounts)
+- [Uniswap V2 Subgraph](https://thegraph.com/explorer/subgraph/uniswap/uniswap-v2) query explorer
+  - [Explorer \- GitHub Docs](https://docs.github.com/en/free-pro-team@latest/graphql/overview/explorer) for pretty-printing
+  - [Graph Docs](https://thegraph.com/docs/graphql-api#schema)
+- [uniswap\-v2\-subgraph/schema\.graphql](https://github.com/Uniswap/uniswap-v2-subgraph/blob/master/schema.graphql) source code
+- [Uniswap/uniswap\-info: ℹ️ Uniswap analytics\.](https://github.com/Uniswap/uniswap-info)
+- [uniswap\-info/index\.js at 932efce769c353bde087d2dfd2a448bd5d19a3c2 · Uniswap/uniswap\-info](https://github.com/Uniswap/uniswap-info/blob/932efce769c353bde087d2dfd2a448bd5d19a3c2/src/components/PositionList/index.js#L141-L142)
+- [SQL \- GnuCash](https://wiki.gnucash.org/wiki/SQL)
+- [MySQL :: MySQL 5\.7 Reference Manual :: 11\.5 The JSON Data Type](https://dev.mysql.com/doc/refman/5.7/en/json.html)
--- packages/unifetch/src/script.js
+++ packages/unifetch/src/script.js
@@ -1,14 +1,152 @@
 // @ts-check
 
-export const q1 = `
+export const UniswapAPI = {
+  endPoint: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+  qPositions: `
 query($id:String!){
   user(id: $id) {
-    id
+    liquidityPositions {
+      liquidityTokenBalance
+      pair {
+        totalSupply
+        reserveUSD
+        token0 {
+          id
+          symbol
+          name
+          decimals
+        }
+        token1 {
+          id
+          symbol
+          name
+          decimals
+        }
+      }
+    }
   }
-}`;
+}`,
+  qTransactions: `
+  query transactions($user: Bytes!) {
+    mints(orderBy: timestamp, orderDirection: desc, where: { to: $user }) {
+      id
+      transaction {
+        id
+        timestamp
+        __typename
+      }
+      pair {
+        id
+        token0 {
+          id
+          symbol
+          __typename
+        }
+        token1 {
+          id
+          symbol
+          __typename
+        }
+        __typename
+      }
+      to
+      liquidity
+      amount0
+      amount1
+      amountUSD
+      __typename
+    }
+    burns(orderBy: timestamp, orderDirection: desc, where: { sender: $user }) {
+      id
+      transaction {
+        id
+        timestamp
+        __typename
+      }
+      pair {
+        id
+        token0 {
+          symbol
+          __typename
+        }
+        token1 {
+          symbol
+          __typename
+        }
+        __typename
+      }
+      sender
+      to
+      liquidity
+      amount0
+      amount1
+      amountUSD
+      __typename
+    }
+    swaps(orderBy: timestamp, orderDirection: desc, where: { to: $user }) {
+      id
+      transaction {
+        id
+        timestamp
+        __typename
+      }
+      pair {
+        token0 {
+          symbol
+          __typename
+        }
+        token1 {
+          symbol
+          __typename
+        }
+        __typename
+      }
+      amount0In
+      amount0Out
+      amount1In
+      amount1Out
+      amountUSD
+      to
+      __typename
+    }
+  }
+`,
+};
 
 const { freeze } = Object;
 const { log } = console;
+
+/**
+ * @param {{
+ *   $: typeof document.querySelector,
+ *   fetch: typeof fetch,
+ * }} io
+ */
+export function ui({ $, fetch }) {
+  const gql = GraphQL(UniswapAPI.endPoint, { fetch });
+  const fmt = (obj) => JSON.stringify(obj, null, 2);
+  const amt = (txt) => parseFloat(txt);
+  $("#run").addEventListener("click", (_click) => {
+    const ethAddr = $("#ethAddr").value;
+
+    gql.runQuery(UniswapAPI.qPositions, { id: ethAddr }).then((result) => {
+      $("#result").value = fmt(result);
+      const positions = result.data.user.liquidityPositions.map((position) => {
+        const poolOwnership =
+          amt(position.liquidityTokenBalance) / amt(position.pair.totalSupply);
+        const valueUSD = poolOwnership * amt(position.pair.reserveUSD);
+        console.log({
+          name: `${position.pair.token0.symbol}-${position.pair.token1.symbol}`,
+          valueUSD,
+        });
+      });
+    });
+
+    gql.runQuery(UniswapAPI.qTransactions, { user: ethAddr }).then((result) => {
+      console.log(result);
+    });
+  });
+}
 
 /**
  * @param { string } endPoint
@@ -31,7 +169,7 @@ export function GraphQL(endPoint, { fetch }) {
     try {
       result = await resp.json();
     } catch (err) {
-      console.error({ methodUrl: addr, request, err });
+      console.error({ addr, request, err });
       throw err;
     }
     // Add status if server error
