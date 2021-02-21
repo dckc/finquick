5d4f4d90e0e2b900a3af8f38b05f8fca066ec530
Dan Connolly <dckc@madmode.com>
2021-02-21 13:43:37 -0600

    feat(etherscan): fetch all transactions; save to DB

--- brcal/src/etherscan.js
+++ brcal/src/etherscan.js
@@ -1,7 +1,7 @@
 // @ts-check
 
-const { check } = require('./check');
-const { WebApp } = require('./WebApp');
+import { check } from './check';
+import { WebApp } from './WebApp';
 import { GCBook } from './gcbook';
 
 const { freeze } = Object;
@@ -34,192 +34,80 @@ const tx0 = {
 
 /**
  * @param {ReturnType<typeof import('./WebApp').WebApp> } web
- * @param {string} apiKey
+ * @param {string} apikey
  *
  * @typedef { import('./WebApp').Query } Query
  */
-function makeEtherscan(web, apiKey) {
-  function txlist(/** @type { string } */ address) {
-    const params = {
-      apikey: apiKey,
-      module: 'account',
-      action: 'txlist',
-      address: address,
-    };
-    return paged(params, tx0);
-  }
-
-  function txlistinternal(/** @type { string } */ address) {
-    const params = {
-      apikey: apiKey,
-      module: 'account',
-      action: 'txlistinternal',
-      address: address,
-    };
-    return paged(params, tx0);
-  }
-
+function makeEtherscan(web, apikey) {
   /**
    * @param {Query} params
    * @param {T} _template
-   * @returns {Promise<{
-   *   status: string,
-   *   message: string,
-   *   result: T[],
-   * }>}
+   * @returns {Promise<T[]>}
    * @template T
    */
   async function call(params, _template) {
     const body = await web.query(params).get();
-    return JSON.parse(body);
+    const info = JSON.parse(body);
+    if (!['0', '1'].includes(info.status)) throw Error(info.message);
+    if (info.error) throw Error(info.error.message || info.error);
+    return info.result;
   }
 
   /**
    * @param {Query} params
-   * @param {T} template
+   * @param {T} ex
    * @returns {Promise<T[]>}
    * @template T
    */
-  async function paged(params, template) {
+  async function paged(params, ex) {
     /** @type {T[][]} */
     let pages = [];
     let page = 1;
-    let qty = 1;
-    let pagedParams;
-    while (qty) {
-      pagedParams = { ...params, page: `${page}`, offset: 128 };
-      const x = await call(pagedParams, template);
-      if (!x.result.length) {
-        break;
-      }
-      pages.push(x.result);
-      qty = x.result.length;
+    for (;;) {
+      const items = await call({ ...params, page: `${page}`, offset: 128 }, ex);
+      if (!items.length) break;
+      pages.push(items);
       page += 1;
     }
     return pages.flat();
   }
 
-  function ETH(/** @type { string } */ address) {
-    function transactions() {
-      async function list() {
-        const txData = await txlist(address); // ISSUE: error handling
-        const txiData = await txlistinternal(address);
-        const byHash = {};
-        txData.concat(txiData).forEach(function(tx) {
-          var row = byHash[tx.hash];
-          if (!row) {
-            row = {};
-            byHash[tx.hash] = row;
-          }
-          row[tx.gasPrice > '' ? 'normal' : 'internal'] = tx;
-          row.hash = tx.hash;
-          row.timeStamp = parseInt(tx.timeStamp);
-        });
-        const rows = values(byHash).sort((a, b) => a.timeStamp - b.timeStamp);
-        return rows;
-      }
-
-      function unpack(txData) {
-        const wei = 1.0 / Math.pow(10, 18);
-        function unpack1(txs) {
-          const tx = 'internal' in txs ? txs.internal : txs.normal;
-          const parent = txs.normal;
-          return {
-            hash: tx.hash,
-            dt: new Date(parseInt(tx.timeStamp) * 1000),
-            value: parseInt(tx.value) * wei,
-            sign: tx.to == address ? 1 : -1,
-            txFee:
-              parent && parent.from == address
-                ? parseInt(parent.gasUsed) * parseInt(parent.gasPrice) * wei
-                : 0,
-            acct2: tx.to == address ? tx.from : tx.to,
-          };
-        }
-        txData = txData.map(unpack1);
-        const hd = ['hash', 'dt', 'value', 'sign', 'txFee', 'acct2']; // Object.keys(txData[0]);
-        const rows = txData.map(function(tx) {
-          return hd.map(function(k) {
-            return tx[k];
-          });
-        });
-        return { hd: hd, rows: rows };
-      }
-
-      return freeze({ list, unpack });
-    }
-    return freeze({ transactions });
-  }
-
-  /**
-   * @param {string} contractaddress
-   * @param {string} address
-   */
-  function token(contractaddress, address) {
-    function transactions() {
-      async function list() {
-        const params = {
-          apikey: apiKey,
-          module: 'account',
-          action: 'tokentx',
-          contractaddress: contractaddress,
-          address: address,
-        };
-        const out = await call(params, tx0);
-        return out.result; // ISSUE: error handling
-      }
-
-      function unpack(txData) {
-        const hd = [
-          'hash',
-          'dt',
-          'value',
-          'tokenDecimal',
-          'tokenSymbol',
-          'sign',
-          'acct2',
-        ];
-
-        const rows = txData.map(function(tx) {
-          const out = {
-            hash: tx.hash,
-            dt: new Date(parseInt(tx.timeStamp) * 1000),
-            value: parseInt(tx.value),
-            tokenDecimal: parseInt(tx.tokenDecimal),
-            tokenSymbol: tx.tokenSymbol,
-            sign: tx.to == address ? 1 : -1,
-            acct2: tx.to == address ? tx.from : tx.to,
-          };
-          return hd.map(function(k) {
-            return out[k];
-          });
-        });
-
-        return { hd, rows };
-      }
-
-      return freeze({ list, unpack });
-    }
-
-    return freeze({ transactions });
-  }
-
-  return freeze({ ETH, token });
+  return freeze({
+    account(/** @type { string } */ address) {
+      const params = { module: 'account', apikey, address };
+      return freeze({
+        async txlist(
+          /** @type { 'txlist' | 'txlistinternal' } */ action = 'txlist',
+        ) {
+          return paged({ ...params, action }, tx0);
+        },
+        /** @type { (c?: string) => Promise<Tx[]> } */
+        async tokentx(contractaddress) {
+          const action = 'tokentx';
+          /** @type {Query} */
+          const token = contractaddress ? { contractaddress } : {};
+          return paged({ ...params, action, ...token }, tx0);
+        },
+      });
+    },
+  });
 }
 
 /**
  *
  * @param {ReturnType<typeof import('./gcbook').GCBook>} bk
+ * @param {string} table
  * @param {Tx[]} txs
  * @typedef {typeof tx0} Tx
  */
-async function save(bk, txs) {
-  await bk.exec('drop table if exists ERC20'); //@@@
-  await bk.exec(`create table if not exists ERC20 (
+async function save(bk, table, txs) {
+  console.log('saving ', txs.length, 'transactions to ', table);
+  await bk.exec(`drop table if exists ${table}`); //@@@
+  await bk.exec(`create table if not exists ${table} (
     hash varchar(256),
     data JSON
   ) character set=utf8 collate=utf8_general_ci`);
-  await bk.exec(`insert into ERC20(hash, data) values ?`, [
+  await bk.exec(`insert into ${table}(hash, data) values ?`, [
     txs.map(tx => [tx.hash, JSON.stringify(tx)]),
   ]);
 }
@@ -237,11 +125,6 @@ async function main({ env, https, mysql }) {
   const svc = makeEtherscan(WebApp(base, { https }), apiKey);
   const account = check.notNull(env.ETH_ADDR);
 
-  const txSvc = svc.token(RHOC, account).transactions();
-  console.log('fetching ERC20 transaction data...');
-  const txs = await txSvc.list();
-  console.log('found', txs.length);
-
   function mkBook() {
     const connect = () =>
       Promise.resolve(
@@ -256,8 +139,13 @@ async function main({ env, https, mysql }) {
   }
 
   const bk = mkBook();
-  console.log('saving to ERC20 table...');
-  await save(bk, txs);
+  console.log('fetching ERC20 transaction data...');
+  const txs = await svc.account(account).tokentx();
+  await save(bk, 'ERC20', txs);
+  await save(bk, 'ETH', [
+    ...(await svc.account(account).txlist()),
+    ...(await svc.account(account).txlist('txlistinternal')),
+  ]);
   console.log('saved.');
   await bk.close();
 }
