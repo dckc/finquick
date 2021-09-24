3f3c54062a6a6f2c4c6ada96f743d60d877772b6
Dan Connolly <dckc@madmode.com>
2021-09-24 02:11:50 -0500

    feat(lmSync): save categories to GnuCash slots table

--- brcal/src/lmSync.js
+++ brcal/src/lmSync.js
@@ -1,6 +1,10 @@
 // @ts-check
 
+import requireText from 'require-text';
 import { WebApp } from './WebApp';
+import { GCBook } from './gcbook';
+
+const { entries } = Object;
 
 const LunchMoneyAPI = {
   endpoint: 'https://dev.lunchmoney.app',
@@ -13,15 +17,40 @@ const LunchMoneyAPI = {
  * @param { typeof process.env } env
  * @param {{
  *   https: typeof import('https'),
+ *   mysql: typeof import('mysql'),
+ *   require: typeof require,
  * }} io
  */
-export const lmSync = async (args, env, { https }) => {
+export const lmSync = async (args, env, { https, mysql }) => {
   const { LUNCH_MONEY_API_KEY: apiKey } = env;
   const creds = { Authorization: `Bearer ${apiKey}` };
-  console.log(creds);
+
+  function mkBook() {
+    const connect = () =>
+      Promise.resolve(
+        mysql.createConnection({
+          host: env.GC_HOST,
+          user: env.GC_USER,
+          password: env.GC_PASS,
+          database: env.GC_DB,
+        }),
+      );
+    return GCBook(connect, m => requireText(m, require));
+  }
+
   const api = WebApp(LunchMoneyAPI.endpoint, { https }, creds);
-  const categories = await api.pathjoin('/v1/categories').get();
-  console.log(categories);
+  const body = await api.pathjoin('/v1/categories').get();
+  const info = JSON.parse(body);
+  console.log('importing', info.categories.length, 'Lunch Money categories');
+  const bk = mkBook();
+  try {
+    await bk.importSlots(
+      `lunchmoney.app/categories`,
+      info.categories.map(data => ({ id: data.id, data })),
+    );
+  } finally {
+    await bk.close();
+  }
 };
 
 /* global require, module, process */
@@ -32,6 +61,9 @@ if (require.main === module) {
     {
       // eslint-disable-next-line global-require
       https: require('https'),
+      // eslint-disable-next-line global-require
+      mysql: require('mysql'),
+      require,
     },
   ).catch(err => console.error(err));
 }
