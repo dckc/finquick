32bf6ffbb8fa14069834369bd31d0fe49ada073b
Dan Connolly <dckc@madmode.com>
2021-10-10 16:08:15 -0500

    feat: lunch money transactions with pagination

--- brcal/src/lmSync.js
+++ brcal/src/lmSync.js
@@ -38,22 +38,45 @@ export const lmSync = async (args, env, { https, mysql }) => {
     return GCBook(connect, m => requireText(m, require));
   }
 
+  /** @type { (app: ReturnType<WebApp>, kind:string) => Promise<Array<any>> } */
+  const paginate = async (app, kind) => {
+    const pages = [];
+    for (let offset = 0, qty = -1; qty !== 0; offset += qty) {
+      // eslint-disable-next-line no-await-in-loop
+      const page = await app
+        .pathjoin(
+          `/v1/${kind}?start_date=1993-01-01&end_date=2050-01-01&offset=${offset}&limit=128`,
+        )
+        .get();
+      const items = JSON.parse(page)[kind];
+      pages.push(items);
+      qty = items.length;
+      console.log({ offset, qty });
+    }
+    return pages.flat();
+  };
   const api = WebApp(LunchMoneyAPI.endpoint, { https }, creds);
+
   const all = {
     categories: api.pathjoin('/v1/categories').get(),
     assets: api.pathjoin('/v1/assets').get(),
     plaid_accounts: api.pathjoin('/v1/plaid_accounts').get(),
-    transactions: api.pathjoin('/v1/transactions').get(),
   };
 
   const bk = mkBook();
   try {
+    const txKind = 'transactions';
+    const transactions = await paginate(api, txKind);
+    await bk.importSlots(
+      `lunchmoney.app/${txKind}`,
+      transactions.map(data => ({ id: data.id, data })),
+    );
+
     for await (const [kind, req] of entries(all)) {
       console.log('fetching', kind, 'from Lunch Money API');
       const body = await req;
       const info = JSON.parse(body);
       // ISSUE: check for errors
-      // ISSUE: pagination
       await bk.importSlots(
         `lunchmoney.app/${kind}`,
         info[kind].map(data => ({ id: data.id, data })),
