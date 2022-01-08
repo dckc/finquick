892c13633910c8cbb5fcb68e79144cc44d7fdd2d
Dan Connolly <dckc@madmode.com>
2022-01-08 04:37:40 -0600

    feat(gc_load_patch): find currency_guid by mnemonic

--- brcal/src/gc_load_patch.sql
+++ brcal/src/gc_load_patch.sql
@@ -4,12 +4,15 @@ set commodity_guid = (select commodity_guid from accounts where name = 'Assets')
 , non_std_scu = (select non_std_scu from accounts where name = 'Assets')
 ;
 
+-- delete from accounts;
 insert into accounts select * from accounts_load;
 
 update transactions_load
 set num = ''
-  , currency_guid = (select currency_guid from transactions where guid = 'f97ee762c03b47b5a9e1aacc9091effa');
+  , currency_guid = (select guid from commodities where mnemonic = 'USD');
 
+
+-- delete from transactions;
 insert into transactions select * from transactions_load;
 
 update splits_load
@@ -20,4 +23,6 @@ set memo=''
   , quantity_denom = value_denom;
 
 
+-- delete from splits;
 insert into splits select * from splits_load;
+
