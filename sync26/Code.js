0238e9d24e4d66f88bc9e3f9f6146ae589cc0381
Dan Connolly <dckc@madmode.com>
2022-10-28 10:22:27 -0500

    feate: match venmo receipts to lunchmoney transactions
     - update Payee, Note in Google Sheet

--- sync26/Code.js
+++ sync26/Code.js
@@ -21,10 +21,8 @@ function getNote(body) {
   return note;
 }
 
-function loadVenmoReceipts() {
-  console.warn("AMBIENT: SpreadsheetApp");
-  const sheet = SpreadsheetApp.getActive().getSheetByName("Receipts");
-  const hd = [
+const headings = {
+  Receipts: [
     "Date",
     "Message Id",
     "Subject",
@@ -33,7 +31,23 @@ function loadVenmoReceipts() {
     "Note",
     "Account",
     "Payment ID",
-  ];
+  ],
+  Transactions: [
+    "Date",
+    "Id",
+    "Detail",
+    "Payee",
+    "Amount",
+    "Note",
+    "Account",
+    "Category",
+  ],
+};
+
+function loadVenmoReceipts() {
+  console.warn("AMBIENT: SpreadsheetApp");
+  const sheet = SpreadsheetApp.getActive().getSheetByName("Receipts");
+  const hd = headings.Receipts;
   sheet.getRange(1, 1, 1, hd.length).setValues([hd]);
   let row = 2;
   const query = 'from:(venmo.com) subject:completed "Payment ID"';
@@ -115,16 +129,7 @@ function loadLunchMoneyTransactions() {
 
   console.warn("AMBIENT: SpreadsheetApp");
   const sheet = SpreadsheetApp.getActive().getSheetByName("Transactions");
-  const hd = [
-    "Date",
-    "Id",
-    "Detail",
-    "Payee",
-    "Amount",
-    "Note",
-    "Account",
-    "Category",
-  ];
+  const hd = headings.Transactions;
   sheet.getRange(1, 1, 1, hd.length).setValues([hd]);
   let row = 2;
   txs.forEach((tx) => {
@@ -145,4 +150,131 @@ function loadLunchMoneyTransactions() {
       console.log("row", row);
     }
   });
+}
+
+const zip = (xs, ys) => xs.map((x, ix) => [x, ys[ix]]);
+
+function getRowRecord(sheet, row, headings) {
+  const [values] = sheet.getRange(row, 1, 1, headings.length).getValues();
+  const entries = zip(headings, values);
+  return Object.fromEntries(entries);
+}
+
+function getSheetRecords(sheet) {
+  const hd = [];
+  for (
+    let col = 1, name;
+    (name = sheet.getRange(1, col).getValue()) > "";
+    col += 1
+  ) {
+    hd.push(name);
+  }
+  const data = sheet.getRange(2, 1, sheet.getLastRow(), hd.length).getValues();
+  const records = [];
+  for (const values of data) {
+    const entries = zip(hd, values);
+    const record = Object.fromEntries(entries);
+    if (!record[hd[0]]) break;
+    records.push(record);
+  }
+  return { hd, records };
+}
+
+function updateRecord(sheet, hd, row, record) {
+  for (const [name, value] of Object.entries(record)) {
+    const col = hd.findIndex((h) => h === name) + 1;
+    sheet.getRange(row, col).setValues([[value]]);
+  }
+}
+
+const DAY = 24 * 60 * 60 * 1000;
+function daysBetween(start, end) {
+  const dur = end.getTime() - start.getTime();
+  return Math.floor(dur / DAY);
+}
+
+function updateTransactionDetailsFromReceipts() {
+  console.warn("AMBIENT: SpreadsheetApp");
+  const doc = SpreadsheetApp.getActive();
+  const { records: accounts } = getSheetRecords(doc.getSheetByName("Accounts"));
+
+  const { records: receipts } = getSheetRecords(doc.getSheetByName("Receipts"));
+  const { hd: txHd, records: txs } = getSheetRecords(
+    doc.getSheetByName("Transactions")
+  );
+  let dest = txs.length;
+  let startOfLastMatchDate;
+
+  const short = (dt) => dt.toISOString().slice(0, 10);
+
+  for (let row = 2; true; row += 1) {
+    const receipt = receipts[row - 2];
+    if (!receipt.Date) break;
+    // console.log('receipt', row, short(receipt.Date), receipt.Amount);
+
+    const account = accounts.find(
+      (acct) => acct.venmo_name.toLowerCase() === receipt.Account.toLowerCase()
+    );
+
+    if (!account) {
+      console.warn("cannot find account:", row, receipt.Account);
+      continue;
+    }
+    // console.log('account', row, short(receipt.Date), receipt.Amount,
+    //  account.id, account.code, account.account_name);
+
+    let tx;
+    let currentDate;
+    let startOfCurrentDate;
+    for (; dest > 1; dest -= 1) {
+      tx = txs[dest - 2];
+      if (!currentDate || currentDate.getTime() !== tx.Date.getTime()) {
+        currentDate = tx.Date;
+        startOfCurrentDate = dest;
+      }
+      const delta = daysBetween(tx.Date, receipt.Date);
+      // console.log('match?', dest, short(tx.Date), delta, tx.Account, tx.Amount);
+      if (delta < -3) continue; // not there yet
+      if (delta > 3) {
+        tx = null;
+        break; // too far
+      }
+      if (tx.Account !== account.id || tx.Amount !== receipt.Amount) continue;
+      const detail = JSON.parse(tx.Detail);
+      if (detail.original_name === "Venmo") break;
+    }
+    if (dest <= 1 || tx === null) {
+      console.warn("cannot find transaction:", row, receipt);
+      if (daysBetween(receipt.Date, txs[0].Date) > 3) {
+        console.warn(
+          "remaining transactions are too old",
+          row,
+          short(receipt.Date),
+          "<<",
+          short(txs[0].Date)
+        );
+        break;
+      }
+      dest = startOfLastMatchDate;
+      continue;
+    }
+    console.log(
+      "match!",
+      { row, dest },
+      short(receipt.Date),
+      short(tx.Date),
+      tx.Amount,
+      account.name,
+      receipt.Payee,
+      receipt.Note
+    );
+    updateRecord(doc.getSheetByName("Transactions"), txHd, dest, {
+      Modified: 1,
+      Payee: receipt.Payee,
+      Note: receipt.Note,
+    });
+    // When we resume, resume at the beginning of the day.
+    startOfLastMatchDate = startOfCurrentDate;
+    dest = startOfLastMatchDate;
+  }
 }
