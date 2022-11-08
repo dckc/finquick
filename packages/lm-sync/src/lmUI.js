75464377e850b4dfb5cdc26af6a208d7e5b48a97
Dan Connolly <dckc@madmode.com>
2022-11-07 21:24:42 -0600

    feat(lm-sync): display selection of current accounts

--- packages/lm-sync/src/lmUI.js
+++ packages/lm-sync/src/lmUI.js
@@ -2,40 +2,98 @@ const { freeze } = Object;
 const { log } = console;
 
 const maybe = (x, f) => (x ? [f(x)] : []);
+const fmt = obj => JSON.stringify(obj, null, 2);
 
 /**
- * @param {{
- *   $: typeof document.querySelector,
- *   fetch: typeof fetch,
- *   localStorage: typeof localStorage,
- * }} io
+ * @param {Object} io
+ * @param {typeof document.querySelector} io.$
+ * @param {typeof fetch} io.fetch
+ * @param {typeof localStorage} io.localStorage
+ * @param {(tag: string) => Element} io.createElement
+ * @param {(tag: string) => Text} io.createTextNode
+ * @param {() => number} io.clock
  */
-export function ui({ $, fetch, localStorage }) {
-  const fmt = obj => JSON.stringify(obj, null, 2);
-  const endPoint = $('#endpoint').getAttribute('href');
+export function ui({
+  $,
+  fetch,
+  localStorage,
+  createElement,
+  createTextNode,
+  clock,
+}) {
+  /**
+   *
+   * @param {string} tag
+   * @param {Record<string, string>} attrs
+   * @param {(Element|string)[]} children
+   */
+  const elt = (tag, attrs = {}, children = []) => {
+    const it = createElement(tag);
+    Object.entries(attrs).forEach(([name, value]) =>
+      it.setAttribute(name, value),
+    );
+    children.forEach(ch => {
+      if (typeof ch === 'string') {
+        it.appendChild(createTextNode(ch));
+      } else {
+        it.appendChild(ch);
+      }
+    });
+    return it;
+  };
 
-  /** @param {string} k */
-  const storageItem = k => {
-    const val = localStorage.getItem(k);
+  const fields = {
+    endPoint: freeze({
+      get: () => $('#endpoint').getAttribute('href'),
+    }),
+    apiKey: freeze({
+      set: val => ($('#apiKey').value = val),
+      onBlur: h => $('#apiKey').addEventListener('blur', h),
+    }),
+    accounts: freeze({
+      set: val => {
+        const control = $('select[name="account"]');
+        control.replaceChildren();
+        val
+          .filter(({ status }) => status !== 'inactive')
+          .forEach(({ id, name }) =>
+            control.appendChild(elt('option', { value: id }, [name])),
+          );
+      },
+      onClick: h => {
+        $('#updateAccounts').addEventListener('click', h);
+      },
+    }),
+  };
+
+  /** @param {string} key */
+  const storageItem = key => {
+    const k = { d: `d.${key}`, m: `m.${key}` };
+    const init = localStorage.getItem(k.d);
     let current;
-    if (val) {
+    if (init) {
       try {
-        current = JSON.parse(val);
+        current = JSON.parse(init);
       } catch (err) {
         console.error(err);
       }
     }
     return freeze({
       get: () => current,
-      set: v => {
-        current = v;
-        localStorage.setItem(k, JSON.stringify(v));
+      set: val => {
+        current = val;
+        const meta = { lastUpdated: clock() };
+        localStorage.setItem(k.d, JSON.stringify(val));
+        localStorage.setItem(k.m, JSON.stringify(meta));
       },
     });
   };
 
   const keyStore = storageItem('lunchmoney.app#apiKey');
-  const accountStore = storageItem('lunchmoney.app/plaid_accounts');
+
+  const book = {
+    accounts: storageItem('lunchmoney.app/plaid_accounts'),
+  };
 
   /** @param {string} path */
   const remoteData = path => {
@@ -43,23 +101,26 @@ export function ui({ $, fetch, localStorage }) {
       get: async () => {
         const apiKey = keyStore.get();
         const headers = { Authorization: `Bearer ${apiKey}` };
-        const url = new URL(path, endPoint);
+        const url = new URL(path, fields.endPoint.get());
         const resp = await fetch(url, { headers });
         return resp.json();
       },
     });
   };
 
-  maybe(keyStore.get(), k => ($('#apiKey').value = k));
-  $('#apiKey').addEventListener('blur', ev => keyStore.set(ev.target.value));
+  fields.apiKey.onBlur(ev => keyStore.set(ev.target.value));
 
-  const accounts = remoteData('/v1/plaid_accounts');
-  maybe(accountStore.get(), accts => ($('#result').value = fmt(accts)));
+  const remote = {
+    accounts: remoteData('/v1/plaid_accounts'),
+  };
 
-  $('#fetch').addEventListener('click', _click => {
-    accounts.get().then(data => {
-      accountStore.set(data);
-      $('#result').value = fmt(data);
+  fields.accounts.onClick(_click => {
+    remote.accounts.get().then(({ plaid_accounts: accts }) => {
+      book.accounts.set(accts);
+      fields.accounts.set(accts);
     });
   });
+
+  maybe(keyStore.get(), k => fields.apiKey.set(k));
+  maybe(book.accounts.get(), accts => fields.accounts.set(accts));
 }
