216f2e27667c500310066150628a477d95bbbaef
Dan Connolly <dckc@madmode.com>
2023-02-06 00:25:19 -0600

    feat(sync26): IBC transfers: build GnuCash transactions (WIP)

--- sync26/tradeSync3.js
+++ sync26/tradeSync3.js
@@ -80,3 +80,87 @@ function loadTradeAccountingMessages() {
   });
   sheet.getRange(2, 1, rows.length, hd.length).setValues(rows);
 }
+
+const zip_ = (xs, ys) => xs.map((x, ix) => [x, ys[ix]]);
+const pairs_ = xs => xs.reduce(
+  ({ done, left }, next, ix) => (
+    ix % 2 === 0 ? { done, left: next} :
+     { done: [...done, [left, next]], left: undefined }), { done: [], left: undefined });
+
+function sheetRecords_(sheet) {
+  const { fromEntries } = Object;
+  const cols = sheet.getLastColumn();
+  const rows = sheet.getLastRow() - 1;
+  const [hd] = sheet.getRange(1, 1, 1, cols).getValues();
+  const detail = sheet.getRange(2, 1, rows, cols).getValues();
+  return detail.map(row => fromEntries(zip_(hd, row)));
+}
+
+/**
+ * pair up records and transform to gnucash format
+ * TODO: amount vs. value, using prices
+ */
+function txfrSplits_(records) {
+  const txs = [];
+  let splits = [];
+  for (const record of records) {
+    // TODO: fee
+    const { txid, url, timestamp,
+      sent_amount, sent_currency,
+      received_amount, received_currency,
+      wallet_address } = record;
+    const currency_name = sent_currency > '' ? sent_currency : received_currency;
+    const amount = sent_currency > '' ? -sent_amount : received_amount;
+    const account = { wallet_address };
+    const split = { guid: { txid }, account, amount, url };
+    splits.push(split);
+    if (splits.length === 1) {
+      const [_all, hash, _msg] = txid.match(/([^-]+)-(.*)/);
+      txs.push({ guid: { hash }, date_posted: timestamp, currency_name, splits })
+    } else {
+      splits = [];
+    }
+  }
+  return txs;
+}
+
+/**
+ * { x: { y: 123 }} => { x_y: 123 }
+ */
+function flatten_(record) {
+  const { entries, fromEntries } = Object;
+  const recur = (pfx, obj) => entries(obj).flatMap(
+    ([k, v]) => (typeof(v) === 'object' && ! (v instanceof Date)) ?
+      recur(`${pfx}${k}_`, v) : [[`${pfx}${k}`, v]]);
+  return fromEntries(recur('', record));
+}
+
+function flattenTxs_(txs) {
+  const { keys } = Object;
+  const records = [];
+  for (const { splits, ...rest } of txs) {
+    records.push(flatten_(rest));
+    splits.forEach(split => records.push(flatten_(split)))
+  }
+  const hd = [...new Set(records.flatMap(r => keys(r)))];
+  const rows = records.map(rec => hd.map(k => rec[k]));
+  return { records, hd, rows };
+}
+
+function setRange(sheet, hd, rows, hdRow = 1) {
+  sheet.getRange(hdRow, 1, 1, hd.length).setValues([hd]);
+  sheet.getRange(hdRow + 1, 1, rows.length, hd.length).setValues(rows);
+}
+
+function buildTradeTransactions() {
+  console.warn('AMBIENT: SpreadsheetApp');
+  const active = SpreadsheetApp.getActive();
+  const stakeTax = active.getSheetByName('StakeTax');
+  const records = sheetRecords_(stakeTax);
+  const splits = records.filter(({ tx_type }) => tx_type === 'TRANSFER');
+  const txs = txfrSplits_(splits);
+  const { hd, rows } = flattenTxs_(txs);
+  console.log('@@@stakeTax', hd, rows)
+  setRange(active.getSheetByName('trading'), hd, rows, 5)
+}
+
