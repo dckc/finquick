227762dae1efa801ce37c06f3dbc5dd1c417b741
Dan Connolly <dckc@madmode.com>
2023-08-26 14:00:49 -0500

    chore: copy secret-tool from Capper work
    2023-02-14 08:25 bd69773 chore: mv ofxies to packages
    2017-10-22 17:46 7a83fad fill in types for exported functions

---
+++ packages/fincaps/src/secret-tool.js
@@ -0,0 +1,70 @@
+/**
+@flow
+*/
+/*eslint-disable no-console */
+
+'use strict';
+
+const Q = require('q');
+
+exports.makeSecretTool = makeSecretTool;
+function makeSecretTool(spawn /*: (string, Array<string>) => Process */) {
+    // cribbed from https://github.com/drudge/node-keychain/blob/master/keychain.js
+    const toolPath = 'secret-tool';
+
+    function lookup(what /*: {[string]: string} */) {
+        const args = ['lookup'];
+        for (let prop in what) {
+            args.push(prop);
+            args.push(what[prop]);
+        }
+
+        console.log('spawn(', toolPath, args, ')');
+        const tool = spawn(toolPath, args);
+
+        let password = '';
+        tool.stdout.on('data', d => { password += d; });
+
+        const out = Q.defer();
+        tool.on('close', (code /* , signal */) => {
+            if (code !== 0) {
+                return out.reject(new Error('non-zero exit from ' + toolPath));
+            }
+
+            out.resolve(password);
+        });
+
+        return out.promise;
+    }
+
+    function makePassKey(tool /*: ({[string]: string}) => Promise<string>*/, properties /*: {[string]: string} */) {
+        return Object.freeze({
+            subKey: (subProps /*: {[string]: string} */) => makePassKey(
+                tool,
+                Object.assign({}, properties, subProps)),
+            properties: () => properties,
+            get: () => tool.lookup(properties)
+        });
+    }
+
+    return Object.freeze({
+        lookup: lookup,
+        makePassKey: makePassKey
+    });
+}
+
+exports.args2props = args2props;
+function args2props(arg1 /*: string*/, args /*: Array<string>*/) /*:Object*/{
+    if (typeof arg1 === 'string') {
+        const properties = {};
+        for (let i=0; i < args.length; i += 1) {
+            let kv = args[i].split('=');  // no destructuring let in node yet?
+            properties[kv[0]] = kv[1];
+        }
+        return properties;
+    } else if (typeof arg1 === 'object' && arg1 !== null) {
+        return arg1;
+    } else {
+        return {};
+    }
+}
