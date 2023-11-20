d779eba6972039abd0f34c7a7bde3cd14b2a37c9
Dan Connolly <dckc@madmode.com>
2023-11-20 00:19:36 -0600

    feat: read vstorage history (for offer status)
     - factor out makeLCD, makeVStorage

--- packages/fincaps/src/batchQuery.js
+++ packages/fincaps/src/batchQuery.js
@@ -26,58 +26,183 @@ const parseIfJSON = d => {
 };
 
 /**
- * @param {string} node
+ * @param {string} apiURL
+ * @param {object} io
+ * @param {typeof fetch} io.fetch
+ */
+export const makeLCD = (apiURL, { fetch }) => {
+  assert.typeof(apiURL, 'string');
+
+  /**
+   * @param {string} href
+   * @param {object} [options]
+   * @param {Record<string, string>} [options.headers]
+   */
+  const getJSON = (href, options = {}) => {
+    const opts = {
+      keepalive: true,
+      headers: {
+        'Content-Type': 'application/json',
+        ...options.headers,
+      },
+    };
+    const url = `${apiURL}${href}`;
+    return fetch(url, opts).then(r => {
+      if (!r.ok) throw Error(r.statusText);
+      return r.json().then(data => {
+        return data;
+      });
+    });
+  };
+
+  return {
+    getJSON,
+    latestBlock: () => getJSON(`/cosmos/base/tendermint/v1beta1/blocks/latest`),
+  };
+};
+/** @typedef {ReturnType<makeLCD>} LCD */
+
+/**
+ * @template T
+ * @param {(value: string) => T} f
+ * @param {AsyncGenerator<string[], void, unknown>} chunks
+ */
+async function* mapHistory(f, chunks) {
+  for await (const chunk of chunks) {
+    for (const value of chunk.reverse()) {
+      yield f(value);
+    }
+  }
+}
+
+/**
+ * @param {LCD} lcd
+ */
+export const makeVStorage = lcd => {
+  const { getJSON } = lcd;
+
+  // height=0 is the same as omitting height and implies the highest block
+  const href = (path = 'published', { kind = 'data' } = {}) =>
+    `/agoric/vstorage/${kind}/${path}`;
+  const headers = height =>
+    height ? { 'x-cosmos-block-height': `${height}` } : undefined;
+
+  const readStorage = (
+    path = 'published',
+    { kind = 'data', height = 0 } = {},
+  ) =>
+    getJSON(href(path, { kind }), { headers: headers(height) }).catch(err => {
+      throw Error(`cannot read ${kind} of ${path}: ${err.message}`);
+    });
+  const readCell = (path, opts) =>
+    readStorage(path, opts)
+      .then(data => data.value)
+      .then(s => (s === '' ? {} : JSON.parse(s)));
+
+  /**
+   * Read values going back as far as available
+   *
+   * @param {string} path
+   * @param {number | string} [minHeight]
+   */
+  async function* readHistory(path, minHeight = undefined) {
+    // undefined the first iteration, to query at the highest
+    let blockHeight;
+    await null;
+    do {
+      // console.debug('READING', { blockHeight });
+      /** @type {string[]} */
+      let values;
+      try {
+        ({ blockHeight, values } = await readCell(path, {
+          kind: 'data',
+          height: blockHeight && Number(blockHeight) - 1,
+        }));
+        // console.debug('readAt returned', { blockHeight });
+      } catch (err) {
+        if (err.message.match(/unknown request/)) {
+          // XXX FIXME
+          // console.error(err);
+          break;
+        }
+        throw err;
+      }
+      yield values;
+      // console.debug('PUSHED', values);
+      // console.debug('NEW', { blockHeight, minHeight });
+      if (minHeight && Number(blockHeight) <= Number(minHeight)) break;
+    } while (blockHeight > 0);
+  }
+
+  /**
+   * @template T
+   * @param {(value: string) => T} f
+   * @param {string} path
+   * @param {number | string} [minHeight]
+   */
+  const readHistoryBy = (f, path, minHeight) =>
+    mapHistory(f, readHistory(path, minHeight));
+
+  return {
+    lcd,
+    readStorage,
+    readCell,
+    readHistory,
+    readHistoryBy,
+  };
+};
+
+/** @typedef {ReturnType<typeof makeVStorage>} VStorage */
+
+/**
+ * @param {ReturnType<makeVStorage>} vstorage
  * @param {FromCapData<string>} unmarshal
  * @param {[AgoricChainStoragePathKind, string][]} paths
- * @param {{fetch: typeof fetch}} io
  */
-export const batchVstorageQuery = async (node, unmarshal, paths, { fetch }) => {
-  const urls = paths.map(
-    path => new URL(`${node}/agoric/vstorage/${path[0]}/${path[1]}`).href,
+export const batchVstorageQuery = async (vstorage, unmarshal, paths) => {
+  const requests = paths.map(([kind, path]) =>
+    vstorage.readStorage(path, { kind }),
   );
-  const requests = urls.map(url => fetch(url));
 
-  return Promise.all(requests)
-    .then(responseDatas => Promise.all(responseDatas.map(res => res.json())))
-    .then(responses =>
-      responses.map((res, index) => {
-        if (paths[index][0] === 'children') {
-          return [
-            pathToKey(paths[index]),
-            { value: res.children, blockHeight: undefined },
-          ];
-        }
+  return Promise.all(requests).then(responses =>
+    responses.map((res, index) => {
+      if (paths[index][0] === 'children') {
+        return [
+          pathToKey(paths[index]),
+          { value: res.children, blockHeight: undefined },
+        ];
+      }
 
-        if (!res.value) {
-          return [
-            pathToKey(paths[index]),
-            {
-              error: `Cannot parse value of response for path [${
-                paths[index]
-              }]: ${JSON.stringify(res)}`,
-            },
-          ];
-        }
-
-        const data = parseIfJSON(res.value);
-
-        const latestValue =
-          typeof data.values !== 'undefined'
-            ? parseIfJSON(data.values[data.values.length - 1])
-            : parseIfJSON(data.value);
-
-        const unserialized =
-          typeof latestValue.slots !== 'undefined'
-            ? unmarshal(latestValue)
-            : latestValue;
-
+      if (!res.value) {
         return [
           pathToKey(paths[index]),
           {
-            blockHeight: data.blockHeight,
-            value: unserialized,
+            error: `Cannot parse value of response for path [${
+              paths[index]
+            }]: ${JSON.stringify(res)}`,
           },
         ];
-      }),
-    );
+      }
+
+      const data = parseIfJSON(res.value);
+
+      const latestValue =
+        typeof data.values !== 'undefined'
+          ? parseIfJSON(data.values[data.values.length - 1])
+          : parseIfJSON(data.value);
+
+      const unserialized =
+        typeof latestValue.slots !== 'undefined'
+          ? unmarshal(latestValue)
+          : latestValue;
+
+      return [
+        pathToKey(paths[index]),
+        {
+          blockHeight: data.blockHeight,
+          value: unserialized,
+        },
+      ];
+    }),
+  );
 };
