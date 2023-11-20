fbf6c33907bc7feea4134889b607072e62307de4
Dan Connolly <dckc@madmode.com>
2023-11-20 00:21:21 -0600

    feat: executeOffer

--- packages/fincaps/src/smartWallet.js
+++ packages/fincaps/src/smartWallet.js
@@ -3,7 +3,7 @@
  */
 // @ts-check
 
-import { E, Far } from '@endo/far';
+import { Far } from '@endo/far';
 import {
   SigningStargateClient,
   assertIsDeliverTxSuccess,
@@ -19,12 +19,12 @@ import { SwingsetMsgs, SwingsetRegistry, gasPriceStake } from './agoricZone.js';
 import { makeHttpClient } from './httpClient.js';
 import { fromMnemonic } from './hdWallet.js';
 import { makeClientMarshaller } from './marshalTables.js';
-import { batchVstorageQuery } from './batchQuery.js';
+import { batchVstorageQuery, makeLCD, makeVStorage } from './batchQuery.js';
 
 /** @template T @typedef {import('@endo/eventual-send').ERef<T>} ERef<T> */
 /** @typedef {import('@cosmjs/proto-signing').OfflineDirectSigner} OfflineDirectSigner */
-
-export { assertIsDeliverTxSuccess };
+/** @template T @typedef {import('@endo/marshal').FromCapData<T>} FromCapData<T> */
+/** @template T @typedef {import('@endo/marshal').ToCapData<T>} ToCapData<T> */
 
 /**
  * @param {string} address
@@ -38,6 +38,127 @@ export const makeWalletActionMessage = (address, spendAction) => ({
     spendAction,
   },
 });
+
+/**
+ * @param {string} address
+ * @param {import('@agoric/smart-wallet/src/smartWallet').BridgeAction} bridgeAction
+ * @param {ToCapData<unknown>} toCapData
+ */
+const makeWalletMessageBy = (address, bridgeAction, toCapData) => {
+  const spendAction = JSON.stringify(toCapData(harden(bridgeAction)));
+  return makeWalletActionMessage(address, spendAction);
+};
+
+/** @typedef {import('./batchQuery.js').LCD} LCD */
+
+/**
+ * @param {{
+ *   lcd: LCD,
+ *   delay: (ms: number) => Promise<void>,
+ *   period?: number,
+ *   retryMessage?: string,
+ * }} opts
+ * @returns {<T>(l: (b: { time: string, height: string }) => Promise<T>) => Promise<T>}
+ */
+export const pollBlocks = opts => async lookup => {
+  const { delay, lcd, period = 3 * 1000, retryMessage } = opts;
+
+  await null; // separate sync prologue
+
+  for (;;) {
+    const status = await lcd.latestBlock();
+    const {
+      block: {
+        header: { height, time },
+      },
+    } = status;
+    try {
+      // see await null above
+      const result = await lookup({ time, height });
+      return result;
+    } catch (_err) {
+      console.error(
+        time,
+        retryMessage || 'not in block',
+        height,
+        'retrying...',
+      );
+      await delay(period);
+    }
+  }
+};
+
+/**
+ * @param {string|number} id
+ * @param {string} from
+ * @param {object} powers
+ * @param {import('./batchQuery.js').VStorage} powers.vstorage
+ * @param {FromCapData<string>} powers.fromCapData
+ * @param {number|string} [minHeight]
+ *
+ * @returns {Promise<import('@agoric/smart-wallet/src/offers').OfferStatus | undefined>}
+ */
+const findOfferStatus = async (
+  id,
+  from,
+  { vstorage, fromCapData },
+  minHeight = undefined,
+) => {
+  const history = vstorage.readHistoryBy(
+    s => fromCapData(JSON.parse(s)),
+    `published.wallet.${from}`,
+    minHeight,
+  );
+
+  for await (const record of history) {
+    // TODO: mustMatch(record, OfferStatusShape)
+    if (record.updated === 'offerStatus' && record.status.id === id) {
+      return record.status;
+    }
+  }
+
+  return undefined;
+};
+
+/**
+ * Get OfferStatus by id, polling until available.
+ *
+ * @param {string} from
+ * @param {string|number} id
+ * @param {{
+ *   fromCapData: FromCapData<string>;
+ *   delay: (ms: number) => Promise<void>;
+ *   vstorage: import('./batchQuery.js').VStorage;
+ * }} io
+ * @param {number|string} [minHeight]
+ * @param {boolean} [untilNumWantsSatisfied]
+ */
+const pollOffer = async (
+  from,
+  id,
+  io,
+  minHeight,
+  untilNumWantsSatisfied = false,
+) => {
+  let threshold = minHeight;
+  const lookup = async ({ height }) => {
+    const offerStatus = await findOfferStatus(id, from, io, threshold);
+    threshold = height;
+    if (!offerStatus) throw Error('retry');
+    harden(offerStatus);
+    if (untilNumWantsSatisfied && !('numWantsSatisfied' in offerStatus)) {
+      throw Error('retry (no numWantsSatisfied yet)');
+    }
+    return offerStatus;
+  };
+  const retryMessage = 'offer not in wallet at block';
+  const opts = {
+    lcd: io.vstorage.lcd,
+    delay: io.delay,
+    retryMessage,
+  };
+  return pollBlocks(opts)(lookup);
+};
 
 /**
  * @param {OfflineDirectSigner} nearSigner
@@ -66,7 +187,6 @@ export const makeSigningClient = async (nearSigner, rpcClient) => {
       gasPrice: gasPriceStake,
     },
   );
-  //   console.debug('signingClient', keys(signingClient));
 
   return Far('SigningClient', {
     /** @param {Parameters<SigningStargateClient['sign']>} args*/
@@ -87,92 +207,147 @@ export const makeSigningClient = async (nearSigner, rpcClient) => {
   });
 };
 
+/** @param {LCD} lcd */
+const makeQueryKit = lcd => {
+  const m = makeClientMarshaller();
+  const vstorage = makeVStorage(lcd);
+
+  /** @param {['children' | 'data', string][]} paths */
+  const batchQuery = async paths =>
+    batchVstorageQuery(vstorage, m.fromCapData, paths);
+
+  /** @param {string} path */
+  const queryData = async path => {
+    const [[_p, answer]] = await batchQuery([['data', path]]);
+    if (typeof answer === 'string') return answer;
+    if (answer.error) throw Error(answer.error);
+    return answer.value;
+  };
+
+  /** @param {string} path */
+  const queryChildren = async path => {
+    const [[_p, answer]] = await batchQuery([['children', path]]);
+    if (typeof answer === 'string') return answer;
+    if (answer.error) throw Error(answer.error);
+    return answer.value;
+  };
+
+  const nameHubCache = new Map();
+
+  /** @param {string} kind */
+  const lookupKind = async kind => {
+    assert.typeof(kind, 'string');
+    if (nameHubCache.has(kind)) {
+      return nameHubCache.get(kind);
+    }
+    const entries = await queryData(`published.agoricNames.${kind}`);
+    const record = Object.fromEntries(entries);
+    const hub = Far('NameHub', { lookup: name => record[name] });
+    nameHubCache.set(kind, hub);
+    return hub;
+  };
+
+  const invalidate = () => {
+    nameHubCache.clear();
+  };
+
+  /**
+   * @param {string} first
+   * @param {string} kind
+   * @param {string} [name]
+   */
+  const lookup = async (first, kind, name) => {
+    assert.equal(first, 'agoricNames');
+    const hub = await lookupKind(kind);
+    if (!name) return hub;
+    return hub.lookup(name);
+  };
+
+  const query = Far('QueryTool', {
+    batchQuery,
+    queryData,
+    queryChildren,
+    lookup,
+    invalidate,
+    fromCapData: m.fromCapData,
+    toCapData: m.toCapData,
+  });
+
+  return { vstorage, query };
+};
+
 export const make = () => {
+  /** @param {number} ms */
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
   /**
    * @param {string} mnemonic
-   * @param {string} url
+   * @param {string} rpcUrl
    */
-  const makeTxTool = async (mnemonic, url) => {
+  const makeTxTool = async (mnemonic, rpcUrl) => {
     const signer = await fromMnemonic(mnemonic);
-    const rpcClient = makeHttpClient(url, fetch);
+    const rpcClient = makeHttpClient(rpcUrl, fetch);
     return makeSigningClient(signer, rpcClient);
   };
 
-  const makeQueryTool = apiUrl => {
-    /** @param {string} url */
-    const getJSON = url =>
-      fetch(url).then(r => {
-        if (!r.ok) throw Error(r.statusText);
-        return r.json();
-      });
+  /**
+   * @param {string} mnemonic
+   * @param {string} rpcUrl
+   * @param {string} apiUrl
+   */
+  const makeWalletKit = async (mnemonic, rpcUrl, apiUrl) => {
+    const rpcClient = makeHttpClient(rpcUrl, fetch);
+    const signer = await fromMnemonic(mnemonic);
+    const tx = await makeSigningClient(signer, rpcClient);
 
-    const m = makeClientMarshaller();
+    const [{ address }] = await signer.getAccounts();
 
-    /** @param {['children' | 'data', string][]} paths */
-    const batchQuery = async paths =>
-      batchVstorageQuery(apiUrl, m.fromCapData, paths, { fetch });
+    const lcd = makeLCD(apiUrl, { fetch });
+    const { vstorage, query } = makeQueryKit(lcd);
+    const { fromCapData, toCapData } = query;
+    const powers = { fromCapData, vstorage, delay };
 
-    /** @param {string} path */
-    const queryData = async path => {
-      const [[_p, answer]] = await batchQuery([['data', path]]);
-      if (typeof answer === 'string') return answer;
-      if (answer.error) throw Error(answer.error);
-      return answer.value;
-    };
-
-    /** @param {string} path */
-    const queryChildren = async path => {
-      const [[_p, answer]] = await batchQuery([['children', path]]);
-      if (typeof answer === 'string') return answer;
-      if (answer.error) throw Error(answer.error);
-      return answer.value;
-    };
-
-    const nameHubCache = new Map();
-
-    /** @param {string} kind */
-    const lookupKind = async kind => {
-      assert.typeof(kind, 'string');
-      if (nameHubCache.has(kind)) {
-        return nameHubCache.get(kind);
-      }
-      const entries = await queryData(`published.agoricNames.${kind}`);
-      const record = Object.fromEntries(entries);
-      const hub = Far('NameHub', { lookup: name => record[name] });
-      nameHubCache.set(kind, hub);
-      return hub;
-    };
-
-    const invalidate = () => {
-      nameHubCache.clear();
+    /**
+     * @param {import('@agoric/smart-wallet/src/offers').OfferSpec} offer
+     * @param {*} [fee]
+     */
+    const sendOffer = async (offer, fee = 'auto') => {
+      const m0 = makeWalletMessageBy(
+        address,
+        { method: 'executeOffer', offer },
+        toCapData,
+      );
+      const trx = await tx.signAndBroadcast(address, [m0], fee);
+      assertIsDeliverTxSuccess(trx);
+      const { transactionHash, height } = trx;
+      return { transactionHash, height };
     };
 
     /**
-     * @param {string} first
-     * @param {string} kind
-     * @param {string} [name]
+     * @param {import('@agoric/smart-wallet/src/offers').OfferSpec} offer
+     * @param {*} [fee]
      */
-    const lookup = async (first, kind, name) => {
-      assert.equal(first, 'agoricNames');
-      const hub = await lookupKind(kind);
-      if (!name) return hub;
-      return hub.lookup(name);
+    const executeOffer = async (offer, fee = 'auto') => {
+      const txInfo = await sendOffer(offer, fee);
+      console.log({ offerId: offer.id, ...txInfo });
+      const status = await pollOffer(address, offer.id, powers);
+      return { tx: txInfo, status };
     };
 
-    return Far('QueryTool', {
-      latestBlock: () =>
-        getJSON(`${apiUrl}/cosmos/base/tendermint/v1beta1/blocks/latest`),
-      batchQuery,
-      queryData,
-      queryChildren,
-      lookup,
-      invalidate,
+    const smartWallet = Far('SmartWallet', {
+      sendOffer,
+      /** @param {string} id */
+      pollOffer: id => pollOffer(address, id, powers),
+      executeOffer,
     });
+
+    return { tx, query, smartWallet };
   };
 
   return Far('SmartWalletFactory', {
     // makeSigningClient,
     makeTxTool,
-    makeQueryTool,
+    makeQueryKit,
+    makeWalletKit,
   });
 };
---
+++ packages/fincaps/test/test-smartWallet.js
@@ -0,0 +1,88 @@
+// @ts-check
+import '@endo/init';
+import anyTest from 'ava';
+
+import { captureIO, replayIO } from './replayFetch.js';
+import { make as makeWalletFactory } from '../src/smartWallet.js';
+import { E } from '@endo/far';
+
+/** @type {import('ava').TestFn<Awaited<ReturnType<typeof makeTestContext>>>} */
+const test = /** @type {any} */ (anyTest);
+
+const RECORDING = false;
+
+const acct = {
+  validator: {
+    address: 'agoric14pfrxg63jn6ha0cp6wxkm4nlvswtscrh2pymwm',
+    mnemonic:
+      'sunny slam blast income daring give vote goat bid response beauty delay grunt payment volcano guess crime organ repair that enroll logic toilet defy',
+  },
+  user1: {
+    address: 'agoric1a3zu5aqw255q0tuxzy9aftvgheekw2wedz3xwq',
+    mnemonic:
+      'congress goose visual acid shine view pair fruit chaos boost cigar upgrade certain warrior color omit perfect clutch little bulb divorce split fashion switch',
+  },
+};
+
+const scenario1 = {
+  rpcURL: 'http://localhost:26657',
+  apiURL: 'http://localhost:1317',
+};
+
+const makeTestContext = async () => {
+  return { fetch: globalThis.fetch, recording: RECORDING };
+};
+
+test.before(async t => {
+  t.context = await makeTestContext();
+});
+
+const { keys } = Object;
+
+test('executeOffer', async t => {
+  const { fetch } = t.context;
+
+  const wf = makeWalletFactory();
+
+  const kit = await E(wf).makeWalletKit(
+    acct.user1.mnemonic,
+    scenario1.rpcURL,
+    scenario1.apiURL,
+  );
+  t.deepEqual(keys(kit), ['tx', 'query', 'smartWallet']);
+
+  const { query, smartWallet } = kit;
+
+  const istBrand = await E(query).lookup('agoricNames', 'brand', 'IST');
+
+  const make = (brand, value) => harden({ brand, value });
+
+  const give = {
+    Collateral: make(istBrand, 10n),
+  };
+
+  /** @type {import('@agoric/smart-wallet/src/offers.js').OfferSpec} */
+  const offerSpec = {
+    id: 'reserveAdd1',
+    invitationSpec: {
+      source: 'agoricContract',
+      instancePath: ['reserve'],
+      callPipe: [['makeAddCollateralInvitation', []]],
+    },
+    proposal: { give },
+  };
+
+  const info = await E(smartWallet).executeOffer(offerSpec);
+
+  t.is(typeof info.tx.height, 'number');
+  t.is(typeof info.tx.transactionHash, 'string');
+
+  t.deepEqual(info.status, {
+    id: 'reserveAdd1',
+    invitationSpec: offerSpec.invitationSpec,
+    numWantsSatisfied: 1,
+    payouts: info.status.payouts,
+    proposal: offerSpec.proposal,
+    result: 'added Collateral to the Reserve',
+  });
+});
