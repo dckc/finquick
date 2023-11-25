8b41ed078c990bf9f625007dc8d07d48606628c7
Dan Connolly <dckc@madmode.com>
2023-11-25 12:27:32 -0600

    SQUASHME: test-query

--- packages/ag-trade/test/test-query.js
+++ packages/ag-trade/test/test-query.js
@@ -5,6 +5,7 @@ import anyTest from 'ava';
 import { captureIO, replayIO } from './replayFetch.js';
 import { makeVStorage } from '../src/batchQuery.js';
 import { makeLCD } from '../src/httpClient.js';
+import { web2 } from './web-fixture.js';
 
 /** @type {import('ava').TestFn<Awaited<ReturnType<typeof makeTestContext>>>} */
 const test = /** @type {any} */ (anyTest);
@@ -17,30 +18,32 @@ const scenario1 = {
 };
 
 const makeTestContext = async () => {
-  return { fetch: globalThis.fetch, recording: RECORDING };
+  const { fetch, web } = RECORDING
+    ? captureIO(globalThis.fetch)
+    : { fetch: replayIO(web2), web: new Map() };
+  return { fetch, web, recording: RECORDING };
 };
 
 test.before(async t => {
   t.context = await makeTestContext();
 });
 
-test.only('wallet updates', async t => {
-  const io = t.context;
-  const lcd = makeLCD(scenario1.apiURL, { fetch: io.fetch });
+test('wallet updates', async t => {
+  const { fetch, recording, web } = t.context;
+  const lcd = makeLCD(scenario1.apiURL, { fetch });
   const vstore = makeVStorage(lcd);
   for await (const actual of vstore.readHistory(
     `published.wallet.${scenario1.addr}`,
   )) {
-    t.is(
-      actual,
-      '{"body":"#{\\"status\\":{\\"id\\":\\"reserveAdd1\\",\\"invitationSpec\\":{\\"callPipe\\":[[\\"makeAddCollateralInvitation\\",[]]],\\"instancePath\\":[\\"reserve\\"],\\"source\\":\\"agoricContract\\"},\\"numWantsSatisfied\\":1,\\"proposal\\":{\\"give\\":{\\"Collateral\\":{\\"brand\\":\\"$0.Alleged: IST brand\\",\\"value\\":\\"+10\\"}}},\\"result\\":\\"added Collateral to the Reserve\\"},\\"updated\\":\\"offerStatus\\"}","slots":["board0257"]}',
-    );
+    if (actual === undefined) break;
+    t.true(Array.isArray(actual));
   }
+  recording && t.snapshot(web, 'wallet updates');
 });
 
 test('live vstorage.readStorage', async t => {
-  const io = t.context;
-  const lcd = makeLCD(scenario1.apiURL, { fetch: io.fetch });
+  const { fetch, web, recording } = t.context;
+  const lcd = makeLCD(scenario1.apiURL, { fetch });
   const vstore = makeVStorage(lcd);
   {
     const item = await vstore.readStorage(
@@ -54,4 +57,5 @@ test('live vstorage.readStorage', async t => {
     const cell = JSON.parse(item.value);
     t.deepEqual(Object.keys(cell), ['blockHeight', 'values']);
   }
+  recording && t.snapshot(web, 'readStorage');
 });
