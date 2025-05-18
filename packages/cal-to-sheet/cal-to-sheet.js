52fea7dc18b1aed0c4a612df729d0470ecee5a2a
Dan Connolly <dckc@madmode.com>
2025-05-17 20:07:20 -0500

    refactor(cal-to-sheet): onOpen was moved to menu.js

--- packages/cal-to-sheet/cal-to-sheet.js
+++ packages/cal-to-sheet/cal-to-sheet.js
@@ -1,11 +1,3 @@
-/**
- * Adds a custom menu to the spreadsheet.
- */
-function onOpen() {
-  SpreadsheetApp.getUi()
-    .createMenu('Calendar Sync')
-    .addItem('Update from Calendar', 'updateSheetFromCalendar')
-    .addToUi();
 }
 
 /**
