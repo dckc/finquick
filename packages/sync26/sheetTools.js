8d2f5012dd965a14d458efd956d1b2bd493b2578
Dan Connolly <dckc@madmode.com>
2025-05-28 22:50:31 -0500

    WIP: packages/sync26/sheetTools.js

--- packages/sync26/sheetTools.js
+++ packages/sync26/sheetTools.js
@@ -4,7 +4,14 @@ function GetAllSheetNames() {
   return sheets.map(sheet => sheet.getName());
 }
 
-function setRange(sheet, hd, rows, hdRow = 1, detailRow = hdRow + 1) {
+/**
+ * @param {GoogleAppsScript.Spreadsheet.Spreadsheet} sheet
+ * @param {string[]} hd
+ * @param {string[][]} rows
+ * @param {number} [hdRow]
+ * @param {number} [detailRow]
+ */
+export function setRange(sheet, hd, rows, hdRow = 1, detailRow = hdRow + 1) {
   sheet.getRange(hdRow, 1, 1, hd.length).setValues([hd]);
   sheet.getRange(detailRow, 1, rows.length, hd.length).setValues(rows);
 }
